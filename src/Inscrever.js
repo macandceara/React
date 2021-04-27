8ff52ce62f2be2098e849f5cf760016677bbd835
macandceara <danielmacedo.ceara@gmail.com>
2021-04-27 20:29:50 -0300

    Validaçaomeial

--- src/Inscrever.js
+++ src/Inscrever.js
@@ -1,53 +1,99 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import logo from "./logo_spotify.png";
-import Form from "react-bootstrap/form";
-import Badge from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import "./App.css";
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import './App.css';
 
-function Inscrever() {
-  return (
-    <div class="formulario">
-      <img className="App-logo" src={logo} alt="Logo" />
-      <Form>
-        <Form.Control placeholder="Enter email" />
-        <Form.Control placeholder="Confirme email" />
-        <Form.Control placeholder="Senha" />
-        <Form.Control
-          type="Como devemos chamar voce"
-          placeholder="Como devemos chamar voce"
-        />
-        <Form>
-          {["radio"].map((type) => (
-            <div key={`inline-${type}`} className="mb-3">
-              <Form.Check
-                inline
-                label="Masculino"
-                type={type}
-                id={`inline-${type}-1`}
-              />
-              <Form.Check
-                inline
-                label="Feminimo"
-                type={type}
-                id={`inline-${type}-2`}
-              />
-              <Form.Check
-                inline
-                label="Não Binario"
-                type={type}
-                id={`inline-${type}-2`}
-              />
-            </div>
-          ))}
-          <Button variant="success" size="lg" Inscrever-se>
-        </Form>
-      </Form>
-      
-      Inscrever-se
-    </Button>
-     
-    </div>
-  );
+const initialState = {
+    name: '',
+    email: '',
+    confEmail: '',
+    password: '',
+    nameError: '',
+    emailError: '',
+    confEmailError: '',
+    passwordError: '',
 }
-export default Inscrever;
+
+class CadastroForm extends React.Component {
+    state = initialState;
+
+    onChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    onSubmit = e => {
+        e.preventDefault();
+        const isValid = this.validate();
+        if(isValid){
+            console.log(this.state)
+            this.setState(initialState)
+        }
+    }
+
+    validate = () =>{
+        let nameError="";
+        let emailError="";
+        let passwordError="";
+        let confEmailError="";
+
+        if(!this.state.name){
+            nameError = "Preencha com um nome válido"
+        }
+        
+        if(!this.state.email.includes('@')){
+            emailError="Email inválido"
+        }
+
+        if (this.state.confEmailError !== this.state.email){
+            confEmailError = "Email Diferente"
+        }
+
+        if(!this.state.password){
+            passwordError = "Senha inválida"
+        }
+
+        if(emailError || nameError || passwordError|| confEmailError){
+            this.setState({emailError, nameError, passwordError,confEmailError});
+            return false;
+        }
+        return true;
+    } 
+
+    render() {
+        return (
+        <form onSubmit={this.handleSubmit}>
+            <Paper className="fundo-form">
+                <h1 className="form-titulo">Login</h1>
+                <Grid container spacing={3}>
+                    <Grid className="form-cadastro__group" item xs={12}>
+                        <label htmlFor="name">Usuário:</label>
+                        <input required id="name" name="name" type="text" onChange={e => this.onChange(e)} value={this.state.name}/>
+                        <div className="error">{this.state.nameError}</div>
+                    </Grid>
+                    <Grid className="form-cadastro__group" item xs={12}>
+                        <label htmlFor="name">Email:</label>
+                        <input required id="email" name="email" type="email" onChange={e => this.onChange(e)} value={this.state.email}/>
+                        <div className="error">{this.state.emailError}</div>
+                    </Grid>
+                    <Grid className="form-cadastro__group" item xs={12}>
+                        <label htmlFor="name">Confimarção de email:</label>
+                        <input required id="confEmail" name="confEmail" type="confEmail" onChange={e => this.onChange(e)} value={this.state.confEmail}/>
+                    </Grid>
+                    <Grid className="form-cadastro__group" item xs={12}>
+                        <label htmlFor="name">Senha:</label>
+                        <input required id="password" name="password" type="password" onChange={e => this.onChange(e)} value={this.state.password}/>
+                        <div className="error">{this.state.passwordError}</div>
+                    </Grid>
+                </Grid>
+                <div>
+                    <button onClick={e => this.onSubmit(e)} className="form-btn" type="submit">Logar</button>
+                </div>
+            </Paper>
+        </form>
+        );
+    }
+}
+
+export default CadastroForm;
